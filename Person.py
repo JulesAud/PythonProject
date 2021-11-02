a5b8d8a69951c63efc452f4c4a886687fde18231
Oguzhan KILIC <shinobi7755@gmail.com>
2021-11-02 16:51:47 +0100

    Ajout
    class Person qui n'est plus à jour. Diagramme class extension mdj et png

--- Person.py
+++ Person.py
@@ -1,38 +1,50 @@
-import random
-import string
 import os
 
 class Person:
     last_name = ""
     first_name = ""
     login = ""
-    password = ""
     hash = ""
-    localisation = ""
-
-    def __init__(self, l_name, f_name, localisation):
-        self.last_name = l_name.toLowerCase()  # nom de famille
-        self.first_name = f_name.toLowerCase()  # prenom
-        self.localisation = localisation
-        self.login = self.first_name[0] + self.last_name
+    workspace = ""
+    mail = ""
 
     def random_char(self):
-        return os.urandom(5)
+        return os.urandom(3)
+
+    #########################
+    def set_login(self, login):
+        self.login = login    
 
     def get_login(self):
         return self.login
 
-    def set_password(self):
-        self.password = self.random_char()
-
-    def get_password(self):
-        return self.password
-
-    def set_hash(self):
-        self.hash = self.get_password().hash()
+    #########################
+    def set_hash(self, password):
+        self.hash = password.hash()
 
     def get_hash(self):
         return self.hash
+    
+    #########################
+    def set_workspace(self, workspace):
+        self.workspace = workspace
+
+    def get_workspace(self):
+        return self.workspace
+
+    #########################                
+    def set_mail(self, mail):
+        self.mail = mail
+
+    def get_mail(self):
+        return self.mail        
+
+    def __init__(self, l_name, f_name, workspace):
+        self.last_name = l_name.toLowerCase()  # nom de famille
+        self.first_name = f_name.toLowerCase()  # prenom
+        self.workspace = workspace
+        self.login = self.first_name[0] + self.last_name
+        self.hash = self.get_hash()
 
     # def to_string(self):
     #   return "The student is "+ self.ge
