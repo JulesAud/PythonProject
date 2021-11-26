f060e6202c4363ed4fb4d4fdbf440addab4ee577
ilyes habki <91946501+ilyeshabki@users.noreply.github.com>
2021-11-26 14:22:23 +0100

    main

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
+from Remote_admin import Remote_admin
 from Site import *
 from Person import *
 
-def menu():
-    choix = int(input("Bienvenu chez Team_Net\n- Saisir 0 pour s'inscrire\n- Saisir 1 pour se connecter\n- Saisir 2 pour quitter le système\nVotre choix: "))
+site_paris = Site("Centre de Paris", "Paris", True)
+site_rennes = Site("Centre de Rennes", "Rennes", False)
+site_strasbourg = Site("Centre de Strasbourg", "Strasbourg", False)
+site_grenoble = Site("Centre de Grenoble", "Grenoble", False)
+#annuaire = []
 
-    while choix<0 or choix>1:
+def inscription(admin,annuaire):
+    nom_de_famille = str(input("Saisissez votre nom de famille: "))
+    prenom = str(input("Saisissez votre prenom: "))
+    mail = str(input("Saisissez votre mail: "))
+    site = str(input("Saisissez p(paris), r(rennes), s(strasbourg), g(grenoble) pour le lieu du site: "))
+    if site == 'p':
+        workspace = site_paris
+
+    elif site == 'r':
+        workspace = site_rennes
+
+    elif site == 's':
+        workspace = site_strasbourg
+
+    elif site == 'g':
+        workspace = site_grenoble
+
+    else:
+        print("Vous n'avaez pas sisi la bonne lettre")
+
+    if admin.isinstance(Remote_admin):
+            admin._create_user(prenom,nom_de_famille,workspace,mail,annuaire)
+    print(annuaire)
+
+def connexion():
+    pass
+
+def modification():
+    pass
+
+def suppression():
+    pass
+
+
+def menu():
+    choix = int(input(
+        "Bienvenu chez Team_Net\n- Saisir 0 pour s'inscrire\n- Saisir 1 pour se connecter\n- Saisir 2 pour quitter le système\nVotre choix: "))
+
+    while choix < 0 or choix > 1:
         choix = int(input("\n- Saisir 0 pour s'inscrire\n- Saisir 1 pour se connecter\nVotre choix: "))
 
     if choix == 0:
@@ -16,46 +58,9 @@ def menu():
     elif choix == 1:
         print("\nConnexion")
         print("-----------")
-        connexion()
-        
+        # connexion()
+
     elif choix == 2:
         print("\nTeam_Net vous remerci pour votre visite\nA bientôt")
 
-def inscription():
-    nom_de_famille = str(input("Saisissez votre nom de famille: "))
-    prenom = str(input("Saisissez votre prenom: "))
-    site = str(input("Saisissez p(paris), r(rennes), s(strasbourg), g(grenoble) pour le lieu du site: "))
-    if site == 'p':
-        workspace = site_paris
-        
-    elif site == 'r':
-        workspace = site_rennes
-    
-    elif site == 's':
-        workspace = site_strasbourg
-        
-    elif site == 'g':
-        workspace = site_grenoble
-        
-    else:
-        print("Vous n'avaez pas sisi la bonne lettre")
-
-    creation_personne = Person(nom_de_famille, prenom, workspace)
-    
-    fichier_csv = open("users.csv","a")
-    fichier_csv.write(creation_personne.to_string()+"\n")
-    print("Inscrit avec succes")
-    fichier_csv.close()
-
-try:
-    fichier_csv = open("users.csv","r")
-except:
-    fichier_csv = open("users.csv","w+")
-
 menu()
-
-
-site_paris = Site("Centre de Paris", "Paris", True)
-site_rennes = Site("Centre de Rennes", "Rennes", False)
-site_strasbourg = Site("Centre de Strasbourg", "Strasbourg", False)
-site_grenoble = Site("Centre de Grenoble", "Grenoble", False)
